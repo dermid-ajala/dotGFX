fdd4700647cded2a17778097d6983b31c59da953
dermid-ajala <dermid_ajala@proton.me>
2025-06-13 08:25:33 +0700

    Add files via upload

---
+++ gfx_dots/ht16k33_GFX/msg/th.js
@@ -0,0 +1,31 @@
+Blockly.Msg.BASIC_LED16X8_GFX_TITLE = "แอลอีดี16X8 แบบจุด";
+Blockly.Msg.BASIC_LED16X8_GFX_TOOLTIP = "แสดงจุดที่เลือกบนแอลอีดี 16X8";
+Blockly.Msg.BASIC_LED16X8_GFX_HELPURL = "";
+
+Blockly.Msg.BASIC_LED16X8_CLR_GFX_TITLE = "ล้างแอลอีดี 16X8";
+Blockly.Msg.BASIC_LED16X8_CLR_GFX_TOOLTIP = "ล้างแอลอีดี 16X8";
+Blockly.Msg.BASIC_LED16X8_CLR_GFX_HELPURL = "";
+
+Blockly.Msg.LED16X8_GFX_TITLE = "แอลอีดี16X8 แบบจุด";
+Blockly.Msg.LED16X8_GFX_TOOLTIP = "แอลอีดี16X8 แบบจุด";
+Blockly.Msg.LED16X8_GFX_HELPURL = "";
+
+Blockly.Msg.GFX_TYPE = "ประเภท";
+
+Blockly.Msg.GFX_TYPE_PLOT = "ลงจุด";
+Blockly.Msg.GFX_TYPE_UNPLOT = "ไม่ลงจุด";
+Blockly.Msg.GFX_TYPE_TOGGLE = "ตรงข้าม";
+Blockly.Msg.GFX_TYPE_BAR = "กราฟแท่ง";
+
+Blockly.Msg.LED16X8_MOVE_TITLE = "แอลอีดี16X8 แบบเคลื่อนที่";
+Blockly.Msg.LED16X8_MOVE_TOOLTIP = "แอลอีดี16X8 แบบเคลื่อนที่";
+Blockly.Msg.LED16X8_MOVE_HELPURL = "";
+
+Blockly.Msg.LED16X8_GFX_STACK_TITLE = "แอลอีดี16X8 แบบจุดซ้อน";
+Blockly.Msg.LED16X8_GFX_STACK_TOOLTIP = "แอลอีดี16X8 แบบจุดซ้อน";
+Blockly.Msg.LED16X8_GFX_STACK_HELPURL = "";
+
+Blockly.Msg.GFX_TYPE_ROTATE = "หมุน";
+Blockly.Msg.GFX_TYPE_SHIFT = "เลื่อน";
+Blockly.Msg.GFX_TYPE_RIGHT = "ขวา";
+Blockly.Msg.GFX_TYPE_LEFT = "ซ้าย";
